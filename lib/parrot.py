62afe3489ef0767676a6e2ee956f700c75543c73
Derek Truesdale <dtruesdale37@gmail.com>
2023-09-02 11:37:31 -0500

    lab completed

--- lib/parrot.py
+++ lib/parrot.py
@@ -1,2 +1,8 @@
-def parrot():
-    pass
+def parrot(variable='no pass'):
+    if variable == 'no pass':
+        squawk = 'Squawk!'
+        print(squawk)
+        return squawk
+    else:
+        print(variable)
+        return variable
